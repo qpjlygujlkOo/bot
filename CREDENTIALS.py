9fcf46d07b3eca0147bd7becd85225d31347a713
qpjlygujlkOo <qpjlygujlkOo@gmail.com>
2021-03-01 16:14:48 +0300

    551

---
+++ CREDENTIALS.py
@@ -0,0 +1,2 @@
+VISION_KEY_PATH = 'OCR-Telegram-Bot-master/bot.json'
+BOT_KEY = '1678035442:AAGo3ksdRLTYbNzhZRyVYVuI-NjDP7JjbXk'
--- bot1.py
+++ bot1.py
@@ -2,11 +2,6 @@ import telebot
 import config
 import time
 import os
-import telegram
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler,MessageHandler, Filters
-import visionocr
-import os
-from CREDENTIALS import BOT_KEY
 
 def main():
     # ...
@@ -15,11 +10,35 @@ def main():
     
 bot = telebot.TeleBot(config.TOKEN)
 
-qa1 = ('@SmirnovIvan, @VIKACHU1337, @Konstantin_Gladun, @Azamat_Gu, @Igor_Kirichek')
+qa1 = ('@VIKACHU1337, @Konstantin_Gladun, @Azamat_Gu, @Igor_Kirichek')
 tl1 = ('@ashutay, @DmitryMatlah')
 back1 = ('@ivleonov, @tshloman, @mmeiko, @impavel, @barysh_vn, @DmitryChernoyarov')
-front1 = ('@Frozenzxc, @mimoniskr, @Spartfin, @chimir')
+front1 = ('@Frozenzxc, @Spartfin, @chimir')
 pm1 = ('@gashimovak, @Becky_Bones, @daria_abramova')
+dnr1 = "на обед"
+dnr2 = "кушать"
+dnr3 = "кушоц"
+dnr4 = "кушац"
+dnr5 = "пойду поем"
+dnr6 = "ушел на обед"
+dnr7 = "ушел пообедать"
+dnr8 = "пообедаю"
+dnr9 = "я с вами"
+dnr10 = "подождите меня"
+dnr11 = "я тоже"
+dnr12 = "я тоже на обед"
+dnr13 = "кушот"
+dnr14 = "кушоть"
+dnr15 = "тоже на обед"
+dnr16 = "обедать"
+dnr17 = "ушёл на обед"
+dnr18 = "покушать"
+dnr19 = "покушаю"
+dnr20 = "тоже покушать"
+dnr21 = "пора покушать"
+
+dnrstc1 = "AgADAgADA8KlDg"
+dnrstc2 = "AgADZgADqregFw"
 
 @bot.message_handler(commands=['qa'])
  
@@ -51,15 +70,14 @@ def mgr(message):
 def all(message):
     bot.send_message(message.chat.id,'ТОВАРИЩИ! \n' + qa1 + ' ' + tl1 + ' ' + front1 + ' ' + back1 + ' ' + pm1.format(message.from_user, bot.get_me()),parse_mode='html')
 
-@bot.message_handler(commands=['митинг'])
+@bot.message_handler(commands=['meet'])
  
 def meetup(message):
-    sti = open('stick\\sticker.webp', 'rb')
+    sti = open('stick/sticker.webp', 'rb')
 
-    bot.send_message(message.chat.id,'МИТИНГ! \n' + qa1 + ' ' + tl1 + ' ' + front1 + ' ' + back1 + ' ' + pm1.format(message.from_user, bot.get_me()),parse_mode='html')
+    bot.send_message(message.chat.id,'МИТИНГ! \n' + qa1 + ' ' + tl1 + ' ' + front1 + ' ' + back1 + ' ' + pm1 + ' \n' 'https://meet.google.com/bxq-nsek-ept'.format(message.from_user, bot.get_me()),parse_mode='html')
     time.sleep(2)
     bot.send_sticker(message.chat.id, sti)
-
 @bot.message_handler(content_types=['text'])
 
 def handle_text(message):
@@ -71,51 +89,5 @@ def handle_text(message):
 def handle_docs_audio(message):
     if message.sticker.file_unique_id == dnrstc1 or message.sticker.file_unique_id == dnrstc2:
         bot.send_message(message.chat.id, f'Приятного аппетита, {message.from_user.first_name}!')
-
-def start(update, context):
-    firstname = update.message.from_user.first_name
-    bot.send_message(chat_id=update.message.chat_id, text=("Hey " + firstname + ", send me any picture containing text and I will OCR it for you!"))
-
-def echo(update, context):
-    message = update.message.text
-    bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
-    bot.send_message(chat_id=update.message.chat_id, text="Try sending me a picture with any text in it and I will OCR it for you!")
-
-def receive_doc(update, context):
-    message = update.message
-    file_id = message.document.file_id
-    chat_id = update.message.chat_id
-    ocr_file(bot,update,file_id,chat_id)
-
-def receive_image(update, context):
-    message = update.message
-    file_id = message.photo[-1].file_id
-    chat_id = update.message.chat_id
-    ocr_file(bot,update,file_id,chat_id)
-    
-def ocr_file(update, context, file_id, chat_id):
-    filepath = os.path.expanduser('~') + '/' + file_id
-    print(filepath)
-    bot.send_message(chat_id=chat_id, text="Please hold on...")
-    bot.send_chat_action(chat_id=chat_id, action=telegram.ChatAction.TYPING)
-    file = bot.get_file(file_id).download(filepath)
-    
-    ocr_text = visionocr.read_image(filepath)
-    bot.send_message(chat_id=chat_id, text='Here you go:\n\n' + str(ocr_text))
-    os.remove(filepath)
-
-
-# handlers
-start_handler = CommandHandler('start', start)
-echo_handler = MessageHandler(Filters.text, echo)
-doc_handler = MessageHandler(Filters.document, receive_doc)
-image_handler = MessageHandler(Filters.photo, receive_image)
-
-#dispatchers
-#тызаебеш
-dispatcher.add_handler(start_handler)
-dispatcher.add_handler(echo_handler)
-dispatcher.add_handler(doc_handler)
-dispatcher.add_handler(image_handler)
 
 bot.polling(none_stop=True)
