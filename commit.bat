a3534e554f299b9fbba8cce59ad6c3c41966b587
qpjlygujlkOo <qpjlygujlkOo@gmail.com>
2022-04-21 18:46:31 +0300

    %time%

--- commit.bat
+++ commit.bat
@@ -1,7 +1,4 @@
-cd C:\bot1\bot\
-git fetch origin master
-git push origin master
-heroku login
-cd C:\bot1\bot\
-git fetch heroku master
-git push heroku master
+$Command =  "bot"
+$Command
+cd C:\bot1\bot\ ; git fetch origin master ; git add . ; git commit -m %time% ; git push origin master ; heroku login ; git fetch heroku master ; git push heroku master
+Invoke-Expression $Command
